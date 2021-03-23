c0f8dd860ca1b07bb587e064eab181b72217f6f0
akarin-git <mwakarin14@gmail.com>
2021-03-23 16:38:02 +0900

    secret

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   target: "serverless",
   env: {
-    API_CLIENT_ID: 2,
-    API_CLIENT_SECRET: "X3KdtsR9BSbVP23dL1DTKhV86CcLcoaqUmdPKdFm",
+    // API_CLIENT_ID: 1,
+    // API_CLIENT_SECRET: "WpoorOcyeJORs63mmDxUbfrYhrrd1Cy5TXRwZh9j",
+    API_CLIENT_ID: 4,
+    API_CLIENT_SECRET: "SDetCK9oI0UFn6cRQaGDFAC17XklGSBp9s1utoAP",
   },
   
 };
