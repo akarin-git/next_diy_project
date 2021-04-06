07ee8b3e6fb39937745cd9dc1ed73a0b5c588ae3
akarin-git <mwakarin14@gmail.com>
2021-04-06 13:06:44 +0900

    heroku add

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-// export const API_ENDPOINT = "https://fabloopdiy.herokuapp.com/";
-export const API_ENDPOINT = "http://localhost:8080";
+export const API_ENDPOINT = "https://fabloopdiy.herokuapp.com/";
+// export const API_ENDPOINT = "http://localhost:8080";
--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   target: "serverless",
   env: {
-    // API_CLIENT_ID: 4,
-    // API_CLIENT_SECRET: "DrnnMscSVthfSRgryQz3fRhtzbSRbkstFeAht2eE",
-    API_CLIENT_ID: 1,
-    API_CLIENT_SECRET: "Rn7H9fwF5g8IVZEHVIXL8wfWgUMn2lBiU6HBxUEv",
+    API_CLIENT_ID: 4,
+    API_CLIENT_SECRET: "MJ89LFsL094HzeqhnTyz3c1BsSq2p330LcST7Ig5",
+    // API_CLIENT_ID: 1,
+    // API_CLIENT_SECRET: "Rn7H9fwF5g8IVZEHVIXL8wfWgUMn2lBiU6HBxUEv",
   },
   future: {
     webpack5: true,
