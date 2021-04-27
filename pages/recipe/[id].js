1cc1bb0d9085f7d4b93725782d0c0ff7aa3fbea8
akarin-git <mwakarin14@gmail.com>
2021-04-28 01:53:58 +0900

    favicon add

--- pages/recipe/[id].js
+++ pages/recipe/[id].js
@@ -116,9 +116,8 @@ export default function Recipe({staticPost,id}) {
                 
             </Container>
 
-            <Box w="60%" pr="10" mt="10" mb="10" m="auto" align="right">
-            {/* ユーザー */}
-            <Box mb="10" w="50%">
+            <Box w={["100%","60%"]} pr="10" mr="10" mt="10" mb="10" m="auto" align="right">
+            <Box mb="10" w={["100%","50%"]} >
               <Link href={`/userpage/${post[0].user_id}`}>
                <Button mt={2} ml="10" w="290px" _hover={{ color: '#ff69b4' }} color={useColorModeValue("gray.600", "gray.200")} rightIcon={<AiOutlineAudit />}>
                      {post[0].user.name}'s DIY recipes page
@@ -136,7 +135,7 @@ export default function Recipe({staticPost,id}) {
                )}
             </Box>
             {/* シェアボタン */}
-            <Flex w={["90%","90%","80%"]}>
+            <Flex w={["90%","90%","80%"]} mb="8">
             <Spacer />
              <Box w="50" align="center" mr={3}>
                  <button onClick={handle.bind(this,post[0].id)}>
